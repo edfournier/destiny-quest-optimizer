382f5014d2c08ec42d8993217d8e891a7e7baad7
Eric Fournier <edfournier@umass.edu>
2025-07-10 11:31:35 -0400

    refactor dashboard

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,22 +1,20 @@
-import { fetchWithAuth } from "@/lib/bungie-auth";
 import { decodeSessionToken } from "@/lib/decode";
 import { cookies } from "next/headers";
 
 export default async function Dashboard() {
     const cookieStore = await cookies();
-    const token = cookieStore.get("access-token")?.value;
-    const session = decodeSessionToken(cookieStore.get("dqo-session-token")?.value || "");
-    if (!token || !session) {
-        return <div>No session found</div>;
+    const token = cookieStore.get("dqo-session-token")?.value;
+    const session = token ? decodeSessionToken(token) : null;
+    if (!session) {
+        return <div>No session!</div>;
     }
 
-    const profile = await fetchWithAuth(
-        token,
-        `https://www.bungie.net/Platform/Destiny2/${session.type}/Profile/${session.sub}/?components=CharacterInventories,Records`
-    );
-
     console.log(session);
-    console.log(profile);
+
+    // TODO:
+    // - in client component, hit `https://www.bungie.net/Platform/Destiny2/${session.type}/Profile/${session.sub}/?components=CharacterInventories,Records`
+    // - in client component, call useDefinitions
+    // - find and display user's bounties, seasonal challenges, etc. 
 
     return (
         <div>
