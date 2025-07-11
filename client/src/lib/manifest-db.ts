b7b1c48ac31c02f21362f0499330a714bdbbc65d
Eric Fournier <edfournier@umass.edu>
2025-07-10 20:26:26 -0400

    fix: manifest caching

--- client/src/lib/manifest-db.ts
+++ client/src/lib/manifest-db.ts
@@ -1,3 +1,4 @@
+import { DestinyManifest } from "bungie-api-ts/destiny2";
 import Dexie, { Table } from "dexie";
 
 class ManifestDB extends Dexie {
@@ -13,38 +14,33 @@ class ManifestDB extends Dexie {
         this.version(1).stores({ definitions: "key" });
     }
 
-    async getLatestManifest(): Promise<Record<string, any>> {
+    async getLatestManifest(): Promise<DestinyManifest> {
         const response = await fetch(`https://www.bungie.net/Platform/Destiny2/Manifest/`);
         if (!response.ok) {
             throw new Error("Bad response from GetManifest");
         }
-
         const data = await response.json();
         return data.Response;
     }
 
     async build(): Promise<this> {
         const manifest = await this.getLatestManifest();
+        console.log(`Fetched manifest (version: ${manifest.version})`);
+        if (manifest.version === localStorage.getItem("manifest-version")) {
+            return this;
+        }
+        localStorage.setItem("manifest-version", manifest.version);
 
+        // Refresh cached definitions for desired keys
         await Promise.all(
             this.keys.map(async (key) => {
                 const path = manifest.jsonWorldComponentContentPaths.en[key];
-                const cached = localStorage.getItem(`path-${key}`);
-                if (path === cached) {
-                    return;
-                }
-
-                // Refresh definitions for this key
                 localStorage.setItem(`path-${key}`, path);
                 const response = await fetch(`https://www.bungie.net${path}`);
                 const definitions = await response.json();
-                return this.definitions.put({
-                    key: key,
-                    value: definitions
-                });
+                return this.definitions.put({ key, value: definitions });
             })
         );
-
         return this;
     }
 
